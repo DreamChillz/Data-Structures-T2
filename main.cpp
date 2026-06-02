b670224a8e59642d3b31005a0b4699c7686d637f
Forroror <loyguang05@gmail.com>
2026-06-02 17:41:40 +0800

    add in case 6
    warehouse need to send location list
    robot_navigation can receive the location list

--- main.cpp
+++ main.cpp
@@ -1,39 +1,73 @@
 #include <iostream>
+#include <string>
 
-// import files here
+// Import all module files here
+#include "order_management.cpp"
 #include "robot_assignment.cpp"
+#include "robot_navigation.cpp" 
 #include "item_management.cpp"
 #include "warehouse_navigation.cpp"
 
 using namespace std;
 
+// Declare menu functions 
 void robotAssignmentMenu();
 void warehouseNavigationMenu();
+void robotNavigationMenu(); 
+// void orderManagementMenu(); // Uncomment when your teammate finishes Task 1
+// void itemManagementMenu();  // Uncomment when your teammate finishes Task 4
 
 int main() {
     int choice;
 
-    do
-    {
+    do {
         cout << "\n--- Warehouse Robot Navigation System ---\n";
         cout << "1. Order Management\n";
         cout << "2. Robot Assignment\n";
         cout << "3. Robot Navigation\n";
         cout << "4. Item Management\n";
         cout << "5. Warehouse Navigation\n";
-        cout << "6. Full System Run (all modules together)\n";
+        cout << "6. Full System Run (Integration Demo)\n";
         cout << "0. Exit\n";
         cout << "Enter choice: ";
         cin >> choice;
 
         switch (choice) {
-
+            case 1:
+                // add orderManagementMenu();
+                break;
             case 2:
                 robotAssignmentMenu();
+                break;
+            case 3: 
+                robotNavigationMenu();
+                break;
+            case 4:
+                // add itemManagementMenu();
                 break;
             case 5:
                 warehouseNavigationMenu();
                 break;
+            case 6: { 
+                cout << "\n=== INITIATING FULL SYSTEM AUTOMATION ===\n"; // needs to add other funciton to complete the system automation 
+                
+                ////change to accept the location list from warehouse
+                cout << "[System] Mainframe calculated route to Shelf A1-1...\n";
+                string locationsFromTask5[] = {"Main Warehouse", "Zone A", "Aisle A1", "Shelf A1-1"}; 
+                int pathLength = 4;
+
+                // send the location to robot_navigation
+                // put the location list from warehouse into the funciton below
+                translateAndLoadPath(locationsFromTask5, pathLength);
+                displayForwardPath();
+
+                // reverse to home 
+                cout << "\n[System] Item retrieved. Executing return protocol...\n";
+                returnUsingReversePath();
+                
+                cout << "\n=== AUTOMATION COMPLETE ===\n";
+                break;
+            }
             case 0:
                 cout << "Exiting system...\n";
                 break;
